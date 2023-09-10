99fa12cd5fc7c2c1b0f9e815b988631b531af5a9
oFamosoRon <ofamosoron@gmail.com>
2023-09-10 19:34:02 -0300

    lint

--- hackerRank-grading-problem.js
+++ hackerRank-grading-problem.js
@@ -4,47 +4,46 @@
 */
 
 function nextMultipleOfFive(item) {
-    let next = Math.floor(item/5)
+  let next = Math.floor(item / 5);
 
-    if(item % 5 > 0) {
-        next++
-    }
-    
-    return next * 5
+  if (item % 5 > 0) {
+    next++;
+  }
+
+  return next * 5;
 }
 
 function gradingStudents(grades) {
-    if(!Array.isArray(grades)) {
-        console.log("wrong type")
-        return 
+  if (!Array.isArray(grades)) {
+    console.log("wrong type");
+    return;
+  }
+
+  var finalGrades = [];
+
+  grades.forEach(function (item) {
+    if (item < 38) {
+      finalGrades.push(item);
+    } else {
+      let nextMultipleOf5 = nextMultipleOfFive(item);
+
+      if (nextMultipleOf5 - item < 3) {
+        finalGrades.push(nextMultipleOf5);
+      } else {
+        finalGrades.push(item);
+      }
     }
 
-    var finalGrades = []
+    console.log(`input: ${item} - output ${finalGrades}`);
+  });
 
-    grades.forEach(function(item){
-        if(item < 38) {
-            finalGrades.push(item)
-        } else {
-            let nextMultipleOf5 = nextMultipleOfFive(item)
-
-            if(nextMultipleOf5 - item < 3) {
-                finalGrades.push(nextMultipleOf5) 
-            } else {
-                finalGrades.push(item)
-            }
-        }
-        
-        console.log(`input: ${item} - output ${finalGrades}`)
-        
-    })
-
-    return finalGrades
+  return finalGrades;
 }
 
 function main() {
-    let grades = [4,73,67,38,33]
+  let grades = [4, 73, 67, 38, 33];
 
-    console.log(gradingStudents(grades))
+  console.log(gradingStudents(grades));
 }
 
-main()
+main();
--- hackerRank-kangaroo-problem.js
+++ hackerRank-kangaroo-problem.js
@@ -1,47 +1,42 @@
 /**
  * https://www.hackerrank.com/challenges/kangaroo/problem
- * 
+ *
  * x1 = 2
  * v1 = 1
- * 
+ *
  * x2 = 1
  * v2 = 2
- * 
+ *
  */
 
-
 function kangaroo(x1, v1, x2, v2) {
-   let result = "NO"
+  let result = "NO";
 
+  if ((x1 > x2 && v1 > v2) || (x2 > x1 && v2 > v1)) {
+    return result;
+  }
 
-   if ((x1 > x2 && v1 > v2) || (x2 > x1 && v2 > v1)) {
-        return result
-   }
+  if (v1 - v2 == 0) {
+    return result;
+  }
 
-   if ((v1 - v2) == 0 ) {
-    return result
-   }
+  let numberOfJumps = (x1 - x2) / (v1 - v2);
 
-   let numberOfJumps = (x1 - x2) / (v1 - v2)
+  if (Number.isInteger(numberOfJumps)) {
+    result = "YES";
+  }
 
-   if(Number.isInteger(numberOfJumps)) {
-    result = "YES"
-   }
-
-   console.log(result)
-   return result
-
+  console.log(result);
+  return result;
 }
 
 function main() {
+  x1 = 0;
+  v1 = 3;
+  x2 = 4;
+  v2 = 2;
 
-   x1 = 0
-   v1 = 3
-   x2 = 4
-   v2 = 2
-
-   kangaroo(x1, v1, x2, v2)
-
+  kangaroo(x1, v1, x2, v2);
 }
 
-main()
+main();
