7f0106cfcc97340e2777c989fa515bd764d3d4b2
oFamosoRon <ofamosoron@gmail.com>
2023-09-14 19:59:03 -0300

    breaking record problem

---
+++ hackerRank-breaking-records-problem.js
@@ -0,0 +1,33 @@
+/**
+ *
+ * https://www.hackerrank.com/challenges/breaking-best-and-worst-records
+ */
+
+function breakingRecords(scores) {
+  let highRecord = scores[0];
+  let lowRecord = scores[0];
+
+  let breakHighRecord = 0;
+  let breakLeastRecord = 0;
+
+  scores.forEach((item) => {
+    if (item > highRecord) {
+      breakHighRecord++;
+      highRecord = item;
+    }
+
+    if (item < lowRecord) {
+      breakLeastRecord++;
+      lowRecord = item;
+    }
+  });
+
+  return [breakHighRecord, breakLeastRecord];
+}
+
+function main() {
+  let scores = [10, 5, 20, 20, 4, 5, 2, 25, 1];
+  console.log(breakingRecords(scores));
+}
+
+main();
